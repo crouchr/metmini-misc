24a05392270741a59d9ca72f28e3814ef5c7cba9
crouchr <richard.crouch@vonage.com>
2021-01-17 16:24:08 +0000

    Initial commit

--- app/get_sunrise_sunset.py
+++ app/get_sunrise_sunset.py
@@ -33,14 +33,19 @@ if __name__ == '__main__':
     my_lat = 51.4146
     my_lon = -1.3749
 
+    print('lat = ' + my_lat.__str__())
+    print('lon = ' + my_lon.__str__())
+
     status_code, response = get_solar_info_api1(my_lat, my_lon)
 
     if status_code != 200:
         print('status_code=' + status_code.__str__())
 
+    print('civil_twilight_begin = ' + response['civil_twilight_begin'])
     print('sunrise = ' + response['sunrise'])
-    print('sunset = ' + response['sunset'])
     print('solar_noon = ' + response['solar_noon'])
+    print('sunset = ' + response['sunset'])
+    print('civil_twilight_end = ' + response['civil_twilight_end'])
     print('day_length = ' + response['day_length'])
 
     print('finished')
