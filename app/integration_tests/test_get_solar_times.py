641a5c0f0ea8ecbbd08136e44679973f24accd4e
crouchr <richard.crouch@vonage.com>
2021-01-17 15:54:05 +0000

    Initial commit

--- app/integration_tests/test_get_solar_times.py
+++ app/integration_tests/test_get_solar_times.py
@@ -10,9 +10,8 @@ def test_get_solar_times():
     """
     query = {}
     query['app_name'] = 'integration_tests'
-    query['lat'] = -1.375
-    query['lon'] = 51.414
-    #query['lon'] = -1.375
+    query['lat'] = 51.414
+    query['lon'] = -1.375
 
     status_code, response_dict = call_rest_api.call_rest_api(integration_definitions.endpoint_base + '/get_solar_times', query)
 
